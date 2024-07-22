44a6135ae8266611a7bb75d8578cd5fc0cdb8263
Kaibalya Mohapatra <kaibalyamohapatra27@gmail.com>
2024-07-22 20:59:53 +0530

    corrected code for deployment

--- app.py
+++ app.py
@@ -2,23 +2,6 @@ from flask import Flask, redirect, request, jsonify,render_template, url_for
 import pickle
 import pandas as pd
 import numpy as np
-from sklearn.base import TransformerMixin, BaseEstimator
-
-# Define the custom transformer
-class ppi(TransformerMixin, BaseEstimator):
-    def fit(self, df, y=None):
-        return self
-
-    def transform(self, df, y=None):
-        splitdf = df['ScreenResolution'].str.split('x', n=1, expand=True)
-        df['X_res'] = splitdf[0]
-        df['Y_res'] = splitdf[1]
-        df['X_res'] = df['X_res'].str.replace(',', '').str.findall(r'(\d+.?\d+)').apply(lambda x: x[0])
-        df['X_res'] = df['X_res'].astype('int')
-        df['Y_res'] = df['Y_res'].astype('int')
-        df['ppi'] = (((df['X_res']**2) + (df['Y_res']**2))**0.5 / (df['Inches'] + 0.0000001)).astype('float')
-        df.drop(columns=['ScreenResolution', 'Inches', 'X_res', 'Y_res'], inplace=True)
-        return df
 
 # Load the model and data
 pipe = pickle.load(open('pipe.pkl', 'rb'))
@@ -45,8 +28,12 @@ def predict():
     IPS=request.form['IPS']
     Cpu_brand=request.form['CPU_Brand']
     os=request.form['os']
-
-    query_df = pd.DataFrame({'Company':[Company],'TypeName':[TypeName],'Inches':[Inches],'ScreenResolution':[ScreenResolution],'Ram':[Ram],'Weight':[Weight],'Touchscreen':[Touchscreen],'IPS':[IPS],'Cpu_brand':[Cpu_brand],'HDD':[HDD],'SSD':[SSD],'Gpu_brand':[Gpu_brand],'os':[os]})
+    
+    splitdf = ScreenResolution.split('x')
+    X_res = int(splitdf[0])
+    Y_res = int(splitdf[1])
+    ppi = float(((X_res**2) + (Y_res**2))**0.5 / (Inches + 0.0000001))
+    query_df = pd.DataFrame({'Company':[Company],'TypeName':[TypeName],'ppi':[ppi],'Ram':[Ram],'Weight':[Weight],'Touchscreen':[Touchscreen],'IPS':[IPS],'Cpu_brand':[Cpu_brand],'HDD':[HDD],'SSD':[SSD],'Gpu_brand':[Gpu_brand],'os':[os]})
     prediction = pipe.predict(query_df)
     predicted_price = int(np.exp(prediction[0]))
     return render_template('index.html', result=predicted_price)
